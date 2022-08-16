48594a92e99dcd2da609b7d6da87ac8c181711dd
tizihoxha <tizihoxha@gmail.com>
2022-08-16 05:53:17 -0700

    added

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * main - function that checks if the number of args is equal to 2
+ * @argc: total number of argument count
+ * @argv: argument vector
+ * Return: 0 , success
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		/** error agrs */
+		open_&_read(argv);
+	return (0);
+}
