c13937bdf7465533a08cd0b1ef72f0d3d905e63c
tizihoxha <tizihoxha@gmail.com>
2022-08-17 14:04:15 -0700

    fixed pchar function

--- print_ch_op.c
+++ print_ch_op.c
@@ -12,13 +12,13 @@ void _pchar(stack_t **top, unsigned int line)
 
 	if (*top == NULL)
 	{
-		fprintf(stderr, "L%u: can't pchar, value out of range\n", line);
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line);
 		exit(EXIT_FAILURE);
 	}
 	num = (*top)->n;
 	if (num < 0 || num > 127)
 	{
-		fprintf(stderr, "L%u: can't pchar, stack empty\n", line);
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line);
 		exit(EXIT_FAILURE);
 	}
 	putchar(num);
