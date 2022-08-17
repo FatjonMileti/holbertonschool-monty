63aee892a42332e92488df2818de345843228472
FatjonMileti <tonimileti@yahoo.com>
2022-08-17 10:47:37 -0700

    add sub func

--- stack2.c
+++ stack2.c
@@ -46,6 +46,46 @@ void _add(stack_t **top, unsigned int line_number)
  *
  */
 
+void _sub(stack_t **top, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*top == NULL || ((*top)->next == NULL))
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*top)->next;
+	temp->n -= (*top)->n;
+	pop_stack(top, line_number);
+}
+
+/**
+ *
+ *
+ *
+ */
+
+void _div(stack_t **top, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*top == NULL || ((*top)->next == NULL))
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*top)->next;
+	temp->n /= (*top)->n;
+	pop_stack(top, line_number);
+}
+
+/**
+ *
+ *
+ *
+ */
+
 void _nop(__attribute__((unused))stack_t **top, __attribute__((unused))unsigned int line_number)
 {
 
