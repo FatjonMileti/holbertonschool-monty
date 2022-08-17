31155a4dd61f9f9d9764f1ad3d3c5fefe8389fc7
tizihoxha <tizihoxha@gmail.com>
2022-08-17 02:29:48 -0700

    fixed swap errors

--- stack2.c
+++ stack2.c
@@ -10,13 +10,13 @@ void _swap(stack_t **top, unsigned int line_number)
 {
 	int nr;
 
-	if (*top == NULL || (*top->next == NULL))
+	if (*top == NULL || ((*top)->next == NULL))
 	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", line);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	nr = (*top)->n;
-	(*top)-> = (*top)->next->n;
+	(*top)->n = (*top)->next->n;
 	(*top)->next->n = nr;
 }
 
